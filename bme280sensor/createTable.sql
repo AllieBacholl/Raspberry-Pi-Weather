2d356c93632473077ed41a410aa1613c1c5ec53d
AllieBacholl <82615224+AllieBacholl@users.noreply.github.com>
2021-12-30 20:37:20 -0600

    Fixes Naming

--- bme280sensor/createTable.sql
+++ bme280sensor/createTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `temperatures` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `temperature` double NOT NULL,
-  `humidity` varchar(20) NOT NULL,
+  `humidity` double NOT NULL,
   `pressure` double NOT NULL,
   `dateMeasured` date NOT NULL,
   `hourMeasured` int(128) NOT NULL,
